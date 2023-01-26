39b2bc1b1624f358c2c10a860bec39c3a226faa4
miakm <mmarkeset@gmail.com>
2023-01-26 11:06:25 +0100

    fiks bruker.py

---
+++ bruker.py
@@ -0,0 +1,31 @@
+class Bruker:
+
+    def __init__(self, brukernavn, passord, epost):
+        self._brukere = []
+        self._brukernavn = brukernavn
+        self._passord = passord
+        self._epost = epost
+        self._saldo = 100000
+
+    def oek_saldo(self, antall):
+        self._saldo += antall
+
+    def hent_bruker(self):
+        return self._brukernavn
+
+    def logg_inn(self):
+        pass
+
+    def ny_bruker(self, brukernavn, passord, epost):
+        bruker = 
+        self._brukere.append(bruker)
+
+
+
+
+
+
+
+
+
+
