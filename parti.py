01489201a4a6828ca89dbc27196e79ccad102573
marcusl04 <marcus.vt.le@gmail.com>
2023-01-26 10:58:46 +0100

    Create parti.py

---
+++ parti.py
@@ -0,0 +1,16 @@
+from politiker import Politiker
+
+class Parti:
+     def __init__(self, verdi, overskrifter):
+        self.verdi = verdi
+        self.overskrifter = overskrifter
+
+
+
+def parti_verdi(self):
+    verdi = politiker[]
+    return(verdi)
+    
+def hent_poeng(self):
+    poeng = overskrifter
+    return(poeng)
