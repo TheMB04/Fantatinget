28bdfe4060ce9b74bdc27f31fe6c4f94ade36f77
TheMB04 <mbskogrand@gmail.com>
2023-01-26 11:32:54 +0100

    update

--- parti.py
+++ parti.py
@@ -1,4 +1,5 @@
 from politiker import Politiker
+import json
 
 class Parti:
      def __init__(self, verdi, overskrifter):
@@ -14,3 +15,13 @@ def parti_verdi(self):
 def hent_poeng(self):
     poeng = overskrifter
     return(poeng)
+
+fil = open("politikere.json", encoding="utf-8")
+politikere = json.load(fil)
+fil.close()
+
+alle_politikere = []
+
+for i in range(0, len(politikere["representanter_oversikt"]["representanter_liste"]["representant"])):
+    politiker = Politiker((politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["fornavn"] + politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["etternavn"]), politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["parti"]["navn"])
+    alle_politikere.append(politiker)   
--- politiker.py
+++ politiker.py
@@ -1,4 +1,3 @@
-import json
 from random import randint
 
 class Politiker:
@@ -47,15 +46,4 @@ class Politiker:
     
     def return_parti(self):
         return self.parti
-
-
-
-fil = open("politikere.json", encoding="utf-8")
-politikere = json.load(fil)
-fil.close()
-
-alle_politikere = []
-
-for i in range(0, len(politikere["representanter_oversikt"]["representanter_liste"]["representant"])):
-    politiker = Politiker((politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["fornavn"] + politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["etternavn"]), politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["parti"]["navn"])
-    alle_politikere.append(politiker)      
+   
