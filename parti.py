39828e50bb0773ef51fb0208fa071dbe4f398e7f
TheMB04 <mbskogrand@gmail.com>
2023-01-26 13:05:20 +0100

    update

--- parti.py
+++ parti.py
@@ -20,21 +20,5 @@ class Parti:
 
 
     def hent_mitt_parti(self):
-        pass
+        pass  
 
-
-
-overskrifter = 0
-
-fil = open("politikere.json", encoding="utf-8")
-politikere = json.load(fil)
-fil.close()
-
-alle_politikere = []
-mine_politikere = []
-
-for i in range(0, len(politikere["representanter_oversikt"]["representanter_liste"]["representant"])):
-    politiker = Politiker((politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["fornavn"] + politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["etternavn"]), politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["parti"]["navn"])
-    alle_politikere.append(politiker)   
-
-mitt_parti = Parti(input("Hva skal partiet ditt hete?"), mine_politikere)
--- verdenstest.py
+++ verdenstest.py
@@ -2,8 +2,20 @@ from stortinget import Verden
 from politiker import Politiker
 from parti import Parti
 from bruker import Bruker
+import json
 
 min_verden = Verden()
+mine_politikere = []
+alle_politikere = []
+
+fil = open("politikere.json", encoding="utf-8")
+politikere = json.load(fil)
+fil.close()
+
+
+for i in range(0, len(politikere["representanter_oversikt"]["representanter_liste"]["representant"])):
+    politiker = Politiker((politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["fornavn"] + politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["etternavn"]), politikere["representanter_oversikt"]["representanter_liste"]["representant"][i]["parti"]["navn"])
+    alle_politikere.append(politiker) 
 
 for politiker in politikerListe:
     ny_politiker = Politiker(navn, parti) 
@@ -13,3 +25,6 @@ for politiker in politikerListe:
 mitt_lag = Parti("Nasjonal Blanding")
 jonas = min_verden.finn_politikere("Jonas Gahr Støre")
 mitt_lag.kjop(jonas)
+
+mitt_parti = Parti(input("Hva skal partiet ditt hete?"), mine_politikere)
+
