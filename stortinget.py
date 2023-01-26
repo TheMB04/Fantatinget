69a42e76ad72d07cdcb2bfdfc188bb337d1eaccd
miakm <mmarkeset@gmail.com>
2023-01-26 11:43:45 +0100

    ikke ferdig

---
+++ stortinget.py
@@ -0,0 +1,16 @@
+class Verden:
+    def __init__(self):
+        self._politikere = []
+        self._lag = []
+
+    def finn_politikere(self, navn):
+        pass
+
+    def finn_lag(self, navn):
+        pass
+    
+    def legg_til_politiker(self, politiker):
+        self._politikere.append(politiker)
+
+    def legg_til_lag(self, lag):
+        self._lag.append(lag)
